a3e09e35608722513c65a73cb37adbf38bf7c868
Rodrigo <rod.maruya@gmail.com>
2025-03-04 19:03:20 +0900

    editando style do hero

--- src/components/hero/index.tsx
+++ src/components/hero/index.tsx
@@ -6,23 +6,18 @@ import { FaWhatsapp } from "react-icons/fa6";
 
 export function Hero() {
   return (
-    <section className="relative ">
-      <div className="relative h-[360px] md:h-[calc(100vh-80px)] bg-black">
-        <Image
-          src={Aspiration}
-          alt="hero"
-          quality={100}
-          priority={true}
-          sizes="(max-width: 768px) 100vw, (max-width: 1200px) 50vw, 33vw"
-          fill
-          className="object-cover opacity-60"
-        />
-      </div>
-      <div className="absolute inset-0 top-24 md:top-1/3 flex flex-col gap-2 max-w-7xl px-2 mx-auto">
-        <h1 className="text-white font-semibold text-left">
+    <section
+      className="relative bg-center bg-cover h-[300px] md:h-[calc(100vh-80px)] flex items-center justify-center"
+      style={{ backgroundImage: `url(${Aspiration.src})` }}
+    >
+      {/* Overlay para escurecer a imagem */}
+      <div className="absolute inset-0 bg-black/50"></div>
+
+      <div className="relative z-10 flex flex-col items-center justify-center gap-2 max-w-7xl px-2 mx-auto h-full">
+        <h1 className="text-white font-semibold text-center">
           Encontre o carro dos seus sonhos!
         </h1>
-        <p className="text-white font-semibold text-left">
+        <p className="text-white font-semibold text-center">
           Ofertas imperdíveis, condições especiais e a melhor seleção de
           veículos para você. Venha conhecer!
         </p>
@@ -31,21 +26,29 @@ export function Hero() {
             href={"#"}
             className="flex items-center justify-center py-2 px-3 bg-[#F17C0E] rounded-md text-white font-bold gap-2 hover:opacity-75 transition-all"
           >
-            <FaWhatsapp size={30} /> Ligue e faça um orçamento!
+            <FaWhatsapp size={24} /> Ligue e faça um orçamento!
           </Link>
         </div>
-      </div>
-      <div className="w-full  absolute bottom-1.5 px-2">
-        <div className="max-w-7xl mx-auto flex items-center justify-start gap-3">
+
+        <div className="max-w-7xl mx-auto flex items-center justify-start gap-3 mt-9">
           <p className="text-white font-bold">Redes Sociais:</p>
-          <div className="flex gap-3 ">
-            <Link href={"#"} className="p-2 rounded-full bg-red-500">
+          <div className="flex items-center justify-center gap-3">
+            <Link
+              href={"#"}
+              className="h-8 w-8 rounded-full bg-red-500 flex items-center justify-center"
+            >
               <BsInstagram size={24} color="#fff" />
             </Link>
-            <Link href={"#"} className="p-2 rounded-full bg-red-500">
+            <Link
+              href={"#"}
+              className="h-8 w-8 rounded-full bg-red-500 flex items-center justify-center"
+            >
               <BsFacebook size={24} color="#fff" />
             </Link>
-            <Link href={"#"} className="p-2 rounded-full bg-red-500">
+            <Link
+              href={"#"}
+              className="h-8 w-8 rounded-full bg-red-500 flex items-center justify-center"
+            >
               <BsTiktok size={24} color="#fff" />
             </Link>
           </div>
